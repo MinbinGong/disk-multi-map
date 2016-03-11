0f8322bd20524619b421c78d207ddfeb5a0fd210
Nikhil Kansal <nkansal96@gmail.com>
2016-03-11 15:00:57 -0800

    modified:   BinaryFile.h

--- BinaryFile.h
+++ BinaryFile.h
@@ -9,55 +9,40 @@
 using namespace std;
 
 template<typename T> struct False : false_type {};
-
 template<typename T>
-struct is_string
-{
+struct is_string {
 	enum { value = is_same<string, typename remove_cv<T>::type>::value };
 };
 
-class BinaryFile
-{
+class BinaryFile {
 public:
-	  // We ought to say
-	  //   typedef ios::streamoff Offset;
-	  // but this would make Offset 8 bytes on most machines.  For the
-	  // purposes of Project 4, a 4-byte type reduces the disk space
-	  // needed to store offsets, yet still allow file sizes of about
-	  // 2GB (not 4GB, because this type should be signed).
-
 	typedef int32_t Offset;
 
-	~BinaryFile()
-	{
+	~BinaryFile() {
 		m_stream.close();
 	}
 
-	bool openExisting(const std::string& filename)
-	{
+	bool openExisting(const std::string& filename) {
 		if (m_stream.is_open())
 			return false;
 		m_stream.open(filename, ios::in|ios::out|ios::binary);
 		return m_stream.good();
 	}
 
-	bool createNew(const std::string& filename)
-	{
+	bool createNew(const std::string& filename) {
 		if (m_stream.is_open())
 			return false;
 		m_stream.open(filename, ios::in|ios::out|ios::binary|ios::trunc);
 		return m_stream.good();
 	}
 
-	void close()
-	{
+	void close() {
 		if (m_stream.is_open())
 			m_stream.close();
 	}
 
 	template<typename T>
-	bool write(const T& data, Offset toOffset)
-	{
+	bool write(const T& data, Offset toOffset) {
 		static_assert(!is_pointer<T>::value && !is_member_pointer<T>::value,
 			"BinaryFile::write can not be used to write a pointer");
 		static_assert(!is_string<T>::value,
@@ -69,15 +54,13 @@ public:
 		return write(reinterpret_cast<const char*>(&data), sizeof(data), toOffset);
 	}
 
-	bool write(const char* data, size_t length, Offset toOffset)
-	{
+	bool write(const char* data, size_t length, Offset toOffset) {
 		return m_stream.seekp(toOffset, ios::beg)  &&
 		       m_stream.write(data, length);
 	}
 
 	template<typename T>
-	bool read(T& data, Offset fromOffset)
-	{
+	bool read(T& data, Offset fromOffset) {
 		static_assert(!is_pointer<T>::value && !is_member_pointer<T>::value,
 			"BinaryFile::read can not be used to read a pointer");
 		static_assert(!is_string<T>::value,
@@ -89,31 +72,27 @@ public:
 		return read(reinterpret_cast<char*>(&data), sizeof(data), fromOffset);
 	}
 
-	bool read(char* data, size_t length, Offset fromOffset)
-	{
+	bool read(char* data, size_t length, Offset fromOffset) {
 		return m_stream.seekg(fromOffset, ios::beg)  &&
 		       m_stream.read(data, length);
 	}
 
 	template<typename T>
-	bool read(const T&, Offset)
-	{
+	bool read(const T&, Offset) {
 		static_assert(False<T>::value,
 			"BinaryFile::read can not be used to read into a const or a temporary object");
 		return false;
 	}
 
 	template<typename T>
-	bool read(T*&&, Offset)
-	{
+	bool read(T*&&, Offset) {
 		static_assert(False<T>::value,
 			"BinaryFile::read can not be used to read a pointer");
 		return false;
 	}
 
-	Offset fileLength()
-	{
-		if ( ! m_stream.is_open())
+	Offset fileLength() {
+		if (!m_stream.is_open())
 			return -1;
 		ios::streamoff currPos = m_stream.tellg();
 		m_stream.seekg(0, ios::end);
@@ -122,15 +101,12 @@ public:
 		return static_cast<Offset>(length);
 	}
 
-	bool isOpen() const
-	{
+	bool isOpen() const {
 		return m_stream.is_open();
 	}
 
 private:
-	fstream m_stream;
-
-	// fstreams are not copyable, so BinaryFiles won't be copyable.
+	fstream m_stream; // fstreams are not copyable, so BinaryFiles won't be copyable.
 };
 
 #endif // BINARYFILE_H_
