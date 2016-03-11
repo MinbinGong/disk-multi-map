ed4a741e11ead467cb42de29bc46fd04b8168d5b
Nikhil Kansal <nkansal96@gmail.com>
2016-03-11 14:55:40 -0800

    new file:   BinaryFile.h 	new file:   DiskMultiMap.cpp 	new file:   DiskMultiMap.h 	new file:   MultiMapTuple.h

---
+++ MultiMapTuple.h
@@ -0,0 +1,13 @@
+#ifndef __HEADER_MULTIMAPTUPLE
+#define __HEADER_MULTIMAPTUPLE
+
+#include <string>
+
+struct MultiMapTuple
+{
+	std::string key;
+	std::string value;
+	std::string context;
+};
+
+#endif // __HEADER_MULTIMAPTUPLE
